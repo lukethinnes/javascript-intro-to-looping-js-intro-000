587354940674d1f95ff618249a9fdc14d87cdceb
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-22 00:50:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,8 @@
 forLoop(let i = 0; i < 25; i++) {
-  if i = 1;
-  console.log("I am 1 strange loop.")
-  else;
-  console.log(`I am ${i} strange loops.`);
+  if i = 1 {
+    console.log("I am 1 strange loop.")
+  }
+  else {
+    console.log(`I am ${i} strange loops.`);
+  }
 }
