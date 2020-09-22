7a20236fd396c80079b447a12684982614aca045
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-22 00:55:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
    for ( let i = 0; i < 25; i++) {
      if (i === 1) {
-       push.array(`I am 1 strange loop.`)
+       array.push(`I am 1 strange loop.`)
      } else {
-       push.array(`I am ${i} strange loops.`)
+       array.push(`I am ${i} strange loops.`)
      }
   }
   return array;
